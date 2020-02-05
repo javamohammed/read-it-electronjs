bcaa4e2efdeaaf2d82a99f1d08317956c4c55c9a
Mido.PHP <java.mohammed@gmail.com>
2020-02-05 11:07:32 +0100

    Add AutoUpdate

---
+++ updater.js
@@ -0,0 +1,6 @@
+const { autoUpdater } = require("electron-updater");
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = "info";
+module.exports = () => {
+    autoUpdater.checkForUpdates();
+}
